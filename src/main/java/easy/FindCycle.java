c747e73aaf44168167f45c5d6137913c51578dc2
elecodev <eric.leconte@gmail.com>
2020-07-29 20:24:05 +0100

    find cycle

---
+++ src/main/java/easy/FindCycle.java
@@ -0,0 +1,31 @@
+package easy;
+
+import utils.ListNode;
+
+public class FindCycle {
+
+
+    public static void main(String args[]) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        n1.next = n2;
+        n2.next = n3;
+        System.out.println(hasCycle(n1));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode fast = head.next;
+        ListNode slow = head;
+
+
+        while (slow != null && fast != null && fast.next != null) {
+            if (slow == fast) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+
+    }
+}
