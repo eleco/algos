e3eed1e247e6d2ad5b0cabc08d6ea74a8e73ec65
elecodev <eric.leconte@gmail.com>
2020-09-02 20:51:30 +0100

    water bottles description

--- src/main/java/easy/WaterBottles.java
+++ src/main/java/easy/WaterBottles.java
@@ -2,6 +2,13 @@ package easy;
 
 public class WaterBottles {
 
+
+    /**
+     * Given numBottles full water bottles, you can exchange numExchange empty water bottles for one full water bottle.
+     * The operation of drinking a full water bottle turns it into an empty bottle.
+     * Return the maximum number of water bottles you can drink.
+     */
+
     public static void main(String arg[]) {
         System.out.println(numWaterBottles(9, 3));
     }
