337aaefb3a0fb968b08660951d2ce7778d388c78
elecodev <eric.leconte@gmail.com>
2020-09-15 21:13:34 +0100

    merge binary trees

---
+++ src/main/java/easy/MergeBinaryTrees.java
@@ -0,0 +1,35 @@
+package easy;
+
+import utils.TreeNode;
+
+public class MergeBinaryTrees {
+
+
+    public static void main(String args[]) {
+
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        n1.left = n2;
+        n1.right = n3;
+        n3.right = n4;
+
+        TreeNode n21 = new TreeNode(10);
+        System.out.println(mergeTrees(n1, n21));
+    }
+
+    public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+        if (t1 != null && t2 != null) {
+            t1.val = t1.val + t2.val;
+            t1.left = mergeTrees(t1.left, t2.left);
+            t1.right = mergeTrees(t1.right, t2.right);
+        } else if (t2 != null) {
+            t1 = t2;
+        }
+
+        return t1;
+    }
+}
+
