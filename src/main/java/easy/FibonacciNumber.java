4752e34b51fe279cbf623312b1abb5b3148debe7
ericl <eric.leconte@gmail.com>
2021-01-08 13:30:27 +0000

    missing number

--- src/main/java/easy/FibonacciNumber.java
+++ src/main/java/easy/FibonacciNumber.java
@@ -2,6 +2,11 @@ package easy;
 
 public class FibonacciNumber {
 
+    /*
+    The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+    such that each number is the sum of the two preceding ones, starting from 0 and 1.
+     */
+
     public static void main(String[] args) {
         System.out.println(fib(10));
     }
