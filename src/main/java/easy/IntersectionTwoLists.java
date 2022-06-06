94beb4134f3d9a885e2c6f3cf0326cbb008990e5
ericl <eric.leconte@gmail.com>
2022-06-06 05:11:00 +0100

    intersection 2 lists

--- src/main/java/easy/IntersectionTwoLists.java
+++ src/main/java/easy/IntersectionTwoLists.java
@@ -2,6 +2,9 @@ package easy;
 
 import utils.ListNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class IntersectionTwoLists {
 
     public static void main(String[] args) {
@@ -10,11 +13,27 @@ public class IntersectionTwoLists {
         ListNode n3 = new ListNode(3);
         n1.next = n2;
         n2.next = n3;
-        System.out.println(getIntersectionNode(n1, n2));
+        System.out.println(getIntersectionNode1(n1, n2));
+        System.out.println(getIntersectionNode2(n1, n2));
     }
 
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
 
-    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> n = new HashSet<>();
+        while(headA!=null) {
+            n.add(headA);
+            headA= headA.next;
+        }
+
+        while(headB!=null) {
+            if (n.contains(headB)) return headB;
+            headB= headB.next;
+        }
+        return null;
+
+    }
+
+    public static ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
 
 
         if (headA==headB) return headA;
