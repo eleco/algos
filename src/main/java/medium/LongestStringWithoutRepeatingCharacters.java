9d6af1b63baf74887f63e73ca8e639a26d64ade6
ericl <eric.leconte@gmail.com>
2022-06-10 07:56:24 +0100

    longest string without repeating characters, solution 2

--- src/main/java/medium/LongestStringWithoutRepeatingCharacters.java
+++ src/main/java/medium/LongestStringWithoutRepeatingCharacters.java
@@ -10,31 +10,71 @@ public class LongestStringWithoutRepeatingCharacters {
      */
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("abcdefgabcddd"));
+
+        System.out.println(Solution1.lengthOfLongestSubstring("abcdefgabcddd"));
+        System.out.println(Solution2.lengthOfLongestSubstring("abcdefgabcddd"));
     }
 
-    static int lengthOfLongestSubstring(String s) {
 
-        if (s == null || s.length() == 0) return 0;
-        if (s.length() == 1) return 1;
+    static class Solution1 {
+        static int lengthOfLongestSubstring(String s) {
 
-        int hi = 0;
-        int lo = 0;
-        Set<Character> set = new HashSet<>();
-        int count = 0;
+            if (s == null || s.length() == 0) return 0;
+            if (s.length() == 1) return 1;
 
-        while (hi < s.length()) {
-            Character c = s.charAt(hi);
-            if (!set.contains(c)) {
-                set.add(c);
-                count = Math.max(count, set.size());
-                hi++;
-            } else {
-                while (set.contains(c)) {
-                    set.remove(s.charAt(lo++));
+            int hi = 0;
+            int lo = 0;
+            Set<Character> set = new HashSet<>();
+            int count = 0;
+
+            while (hi < s.length()) {
+                Character c = s.charAt(hi);
+                if (!set.contains(c)) {
+                    set.add(c);
+                    count = Math.max(count, set.size());
+                    hi++;
+                } else {
+                    while (set.contains(c)) {
+                        set.remove(s.charAt(lo++));
+                    }
                 }
             }
+            return count;
         }
-        return count;
+    }
+
+    static class Solution2 {
+        public static int lengthOfLongestSubstring(String s) {
+            int[] a = new int[128];
+            int start = 0;
+            int max = 0;
+            int i = 0;
+
+            while (i < s.length()) {
+                char c = s.charAt(i);
+                a[c]++;
+
+                if (norepeat(a)) {
+                    max = Math.max(max, i - start + 1);
+
+                } else {
+                    while (!norepeat(a)) {
+                        c = s.charAt(start);
+                        a[c]--;
+                        start++;
+
+                    }
+                }
+                i++;
+            }
+            return max;
+        }
+
+        static boolean norepeat(int[] a) {
+            for (int j : a) {
+                if (j > 1) return false;
+            }
+            return true;
+        }
     }
 }
