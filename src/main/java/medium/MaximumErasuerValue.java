6e83a1363e7c8fa78b07bd32c9c99ba750b02c5e
ericl <eric.leconte@gmail.com>
2022-06-12 08:43:17 +0100

    max erasure value

---
+++ src/main/java/medium/MaximumErasuerValue.java
@@ -0,0 +1,43 @@
+package medium;
+
+public class MaximumErasuerValue {
+
+
+    /*
+    You are given an array of positive integers nums and want to erase a subarray containing unique elements.
+    The score you get by erasing the subarray is equal to the sum of its elements.
+    Return the maximum score you can get by erasing exactly one subarray.
+    An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is,
+    if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+     */
+
+    public static void main(String[] args) {
+        System.out.println(maximumUniqueSubarray(new int[]{4, 2, 4, 5, 6}));
+    }
+
+    static int maximumUniqueSubarray(int[] nums) {
+
+        int max = Integer.MIN_VALUE;
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int[] mem = new int[10001];
+        while (r < nums.length) {
+
+            int c = nums[r];
+            mem[c]++;
+            sum += c;
+
+            while (mem[c] > 1) {
+                mem[nums[l]]--;
+                sum -= nums[l];
+                l++;
+            }
+
+            max = Math.max(max, sum);
+            r++;
+        }
+        return max;
+    }
+}
