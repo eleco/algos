9819bb6e58da5bc6b6050f067af270395ea343c8
ericl <eric.leconte@gmail.com>
2022-07-06 05:51:44 +0100

    fib iterative

--- src/main/java/easy/FibonacciNumber.java
+++ src/main/java/easy/FibonacciNumber.java
@@ -8,12 +8,28 @@ public class FibonacciNumber {
      */
 
     public static void main(String[] args) {
-        System.out.println(fib(10));
+
+        System.out.println(fib_recursive(10));
+        System.out.println(fib_iterative(10));
     }
 
-    private static int fib(int n) {
+    private static int fib_recursive(int n) {
         if (n==0) return 0;
         if (n==1) return 1;
-        return fib(n-1) + fib(n-2);
+        return fib_recursive(n-1) + fib_recursive(n-2);
+    }
+
+    private static int fib_iterative(int n) {
+        int fibn0 = 0;
+        int fibn1 = 1;
+        int fibn = 0;
+        if (n == 0) return fibn0;
+        if (n == 1) return fibn1;
+        for (int i = 2; i <= n; i++) {
+            fibn = fibn0 + fibn1;
+            fibn0 = fibn1;
+            fibn1 = fibn;
+        }
+        return fibn;
     }
 }
