c39411e4858ba20c7248c207bd86ed182bc1b01e
ericl <eric.leconte@gmail.com>
2022-10-02 14:21:59 +0100

    decode ways

---
+++ src/main/java/medium/DecodeWays.java
@@ -0,0 +1,63 @@
+package medium;
+
+public class DecodeWays {
+
+    /*
+    A message containing letters from A-Z can be encoded into numbers using the following mapping:
+
+    'A' -> "1"
+    'B' -> "2"
+    ...
+    'Z' -> "26"
+
+    To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+
+        "AAJF" with the grouping (1 1 10 6)
+        "KJF" with the grouping (11 10 6)
+
+    Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+
+    Given a string s containing only digits, return the number of ways to decode it.
+
+    The test cases are generated so that the answer fits in a 32-bit integer
+     */
+    public static void main(String[] args) {
+        System.out.println(numDecodings("226"));
+    }
+
+
+    static int []mem ;
+    static int numDecodings(String s) {
+        mem = new int[s.length()-1];
+        return rec(s,0);
+    }
+
+    static int rec(String s, int idx) {
+
+        if (idx<s.length() && s.charAt(idx)=='0') {
+            return 0;
+        }
+
+        if (idx==s.length()) {
+            return 1;
+        }
+
+        if (idx==s.length()-1) {
+            return 1;
+        }
+
+        if (mem[idx]!=0) return mem[idx];
+
+        int a= rec (s, idx+1);
+
+        int b =0;
+        if ( (s.charAt(idx)=='2' && s.charAt(idx+1)<'7') || (s.charAt(idx)=='1'))
+            b =rec (s, idx+2);
+
+        mem[idx]=a+b;
+        return a+b;
+
+    }
+
+}
+
